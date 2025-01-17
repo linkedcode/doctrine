a52c5922a13be02fc9bbd1d9bd300e8942a2a71b
Nicolas H. Kosciuk <nhk@linkedcode.com>
2025-01-17 00:37:40 -0300

    Typed return

--- src/Domain/AbstractUser.php
+++ src/Domain/AbstractUser.php
@@ -12,12 +12,12 @@ class AbstractUser
         $this->name = $name;
     }
 
-    public function getId()
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function getName()
+    public function getName(): string
     {
         return $this->name;
     }
