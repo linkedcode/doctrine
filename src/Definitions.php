193b11129292c02f65558688ee80c428e617b052
Nicolas H. Kosciuk <nhk@linkedcode.com>
2025-05-05 14:16:47 -0300

    Ramsey\Uuid\Doctrine\UuidBinaryType

--- src/Definitions.php
+++ src/Definitions.php
@@ -4,15 +4,17 @@ namespace Linkedcode\Doctrine;
 
 use Doctrine\Common\EventManager;
 use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\ORMSetup;
 use Linkedcode\Slim\Settings;
 use Psr\Container\ContainerInterface;
+use Ramsey\Uuid\Doctrine\UuidBinaryType;
 
 class Definitions
 {
-    public static function get(): array
+    public static function get(array $options = []): array
     {
         return [
             EventManager::class => function() {
@@ -21,7 +23,7 @@ class Definitions
             EntityManager::class => function(ContainerInterface $container) {
                 return $container->get(EntityManagerInterface::class);
             },
-            EntityManagerInterface::class => function (ContainerInterface $container) {
+            EntityManagerInterface::class => function (ContainerInterface $container) use ($options) {
                 $appDir = $container->get(Settings::class)->getAppDir();
 
                 $config = ORMSetup::createXMLMetadataConfiguration(
@@ -32,7 +34,14 @@ class Definitions
 
                 $dbParams = $container->get(Settings::class)->get('db');
 
+
                 $connection = DriverManager::getConnection($dbParams, $config);
+
+                if (isset($options['uuid_binary'])) {
+                    Type::addType('uuid_binary', 'Ramsey\Uuid\Doctrine\UuidBinaryType');
+                    $connection->getDatabasePlatform()->registerDoctrineTypeMapping('uuid_binary', 'binary');
+                }
+
                 $entityManager = new EntityManager($connection, $config);
 
                 return $entityManager;
